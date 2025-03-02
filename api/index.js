c359aa3f34143fdad06617ee42fb8ea4648d0bd6
PGX <snakekillah217@gmail.com>
2025-03-02 19:39:32 -0400

    Add files via upload

---
+++ api/index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const PORT = 3002;
+const css = path.join(__dirname, '../static');
+
+app.use(express.static(css));
+app.use(express.urlencoded({ extended: true }));
+
+let nuevo = '';
+let first = `<!DOCTYPE html>
+<html lang="es">
+<head>
+<link href="https://fonts.googleapis.com/css2?family=Lexend:wght@100..900&display=swap" rel="stylesheet">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="/styles.css">
+</head>
+<body>
+<form method="post" action="/cipher">
+<h1>CipherVault</h1>
+<textarea autocomplete="off" name="text" spellcheck="false"></textarea>
+<div id="options">
+<input type="radio" name="opt" value="true" checked/>
+<label>Encriptar</label>
+<input type="radio" name="opt" value="false" />
+<label>Desencriptar</label>
+</div>
+
+<button type="submit">EJECUTAR</button>
+</form>
+`
+let end = `</body></html>`;
+
+app.get('/favicon.ico', (req, res) => {
+ res.status(204).end();
+});
+
+app.get('/', (req, res) => {
+ res.send(`${first}${end}`);
+})
+
+app.post('/cipher', (req, res) => {
+ let texto = req.body.text;
+ let OptSel = req.body.opt;
+ let text0 = '';
+ if(OptSel==='true'){
+  text0=Buffer.from(texto).toString('base64');
+ } else {
+  text0 = Buffer.from(texto, 'base64').toString('utf8');
+ }
+ res.send(`${first}
+ <div id="father-results">
+ <div class="child-results">
+ <pre>${text0}</pre>
+ </div>
+ </div>
+ ${end}
+ <script src="/script.js"></script>`);
+})
+
+module.exports = app;
+
+// app.listen(PORT, () => {
+//     console.log(`ESCUCHANDO POR EL PUERTO => ${PORT}`);
+// })
---
+++ static/script.js
@@ -0,0 +1,18 @@
+function copy() {
+ const preElement = document.querySelector('pre');
+ const textarea = document.createElement('textarea');
+ textarea.value = preElement.textContent;
+ document.body.appendChild(textarea);
+ textarea.select();
+ try {
+  document.execCommand('copy');
+  alert('Texto copiado al portapapeles');
+ } catch (error) {
+  console.error('Error al copiar el texto: ', error);
+  alert('Error al copiar el texto');
+ }
+ document.body.removeChild(textarea);
+}
+document.querySelector('.child-results').addEventListener('click', (event) => {
+copy();
+});
